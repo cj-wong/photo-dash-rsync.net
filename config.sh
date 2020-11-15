2b4131c98ad84218171dad671be85699f9696f0e
CJ Wong <31264514+dark-nova@users.noreply.github.com>
2020-11-15 03:25:58 -0800

    Add config.sh

---
+++ config.sh
@@ -0,0 +1,33 @@
+# shellcheck shell=bash
+#
+# Extra configuration for photo-dash-rsync.net
+
+# Generate the SSH command args to be used.
+# Globals:
+#   None
+# Arguments:
+#   None
+# Returns:
+#   0: if arg generation succeeded
+#   any other integer: depends on the config file
+function config::ssh_args() {
+    local user
+    local host
+    user=$(jq -r '.ssh_user' "${ROOT}/config.json")
+    host=$(jq -r '.ssh_host' "${ROOT}/config.json")
+    # shellcheck disable=SC2034
+    # USER_HOST is used in the main script; no need to export.
+    if [ -n "$user" ]; then
+        USER_HOST="${user}@${host}"
+    else
+        USER_HOST="$host"
+    fi
+}
+
+# Module-level code
+
+if [ -z "${ROOT+x}" ]; then
+    ROOT=$(dirname "${BASH_SOURCE[0]}")
+fi
+
+config::ssh_args
